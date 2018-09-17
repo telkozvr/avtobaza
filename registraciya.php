6907974cc5e06b141b57ed3af0587166307fa849
telkozvr <telkov_aa@nlmk.com>
2018-09-17 17:37:12 +0300

    auth+reg, phone, html

--- registraciya.php
+++ registraciya.php
@@ -3,8 +3,8 @@
 // Подключения модуля соединения с БД 
 require_once "podklyuchenie_k_bd.php";
 
-// Функция для генерации случайной строки необходимой для формирования хеша
-function generateHash($length=6) {
+    // Функция для генерации случайной строки необходимой для формирования хеша
+    function generateHash($length=6) {
     // Символы для формирования строки
     $simvoly = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPRQSTUVWXYZ0123456789";
     // Пустая строка для записи сгенерированных символов
@@ -17,30 +17,52 @@ function generateHash($length=6) {
     }
     return $code;
 }
-
-// Переменные длины почты
+    //Код работает
+    // Переменные длины почты
     $min_dlina_email = 8; //Минимум
     $max_dlina_email = 30; //Максимум
     // Переменные длины пароля
     $min_dlina_parolya = 8;
     $max_dlina_parolya = 30;
+    //В переменную почты записываются данные из поля формы email
+    $email = $_REQUEST["email"];
+    $parol = $_REQUEST["parol"];
+    $povtor_parol = $_REQUEST["povtor_parol"];
+    $phone = $_REQUEST["phone"];
+    //Удаляем из номера всё, кроме цифр
+    $phone = preg_replace('~[^0-9]+~','', $phone);
+    // Массив для ошибок
+    $err = array();
+    // Если была нажата кнопка формы
+    if(isset($_REQUEST["submit"])) {
+        // Сделать галочку при которой поле становится неактивным
+    //Проверка заполненности
+	if(!$email or !$phone) {
+        $err[] = "Введите почту либо номер телефона!";
+    }
+    // Убирать все символы, кроме цифр
 
-// Если была нажата кнопка формы
-if(isset($_POST["submit"])) {
-	// Массив для ошибок
-	$err = array();
-    // Валидация почты на длину символов
-    if(strlen($_POST["email"]) < $min_dlina_email or strlen($_POST["email"]) > $max_dlina_email) {
+    // Проверка длины
+    if(strlen($email) < $min_dlina_email or strlen($email) > $max_dlina_email) {
         $err[] = "Почта должна быть не меньше $min_dlina_email символов и не больше $max_dlina_email";
     } 
-    if(strlen($_POST["parol"]) < $min_dlina_parolya or strlen($_POST["parol"]) > $max_dlina_parolya) {
-        $err[] = "Пароль должен быть не меньше $min_dlina_parolya символов и не больше $max_dlina_parolya";
-    }
     // Присваиваем переменной количество юзеров с указанной в форме почтой
-    $user = mysqli_query($link, "SELECT user_id FROM users WHERE user_email='".mysqli_real_escape_string($link, $_POST["email"])."'");
+    // Сделать такую же проверку для номера телефона
+    $user = mysqli_query($link, "SELECT user_id FROM users WHERE user_email='".mysqli_real_escape_string($link, $email)."'");
     // Если число рядов идентичных user в базе больше 0, то такой юзер уже существует
     if(mysqli_num_rows($user) > 0) {
         $err[] = "Пользователь с такой почтой уже существует в базе данных";
+    }
+    
+    if(!$parol){
+         $err[] = "Введите пароль!";
+    }
+    // Сделать аяксом без перезагрузки
+    if($parol != $povtor_parol){
+        $err[] = "Пароли не совпадают";
+    }
+    if(strlen($parol) < $min_dlina_parolya or strlen($parol) > $max_dlina_parolya) {
+        $err[] = "Пароль должен быть не меньше $min_dlina_parolya символов и не больше $max_dlina_parolya";
     }
 
     // Добавить серверную валидацию пароля
@@ -49,20 +71,17 @@ if(isset($_POST["submit"])) {
 
     // Если ошибок нет
     if(count($err) === 0) {
-        //В переменную почты записываются данные из поля формы email
-        $email = $_POST["email"];
+        $date = date("Y-m-d H:i:s");
         //Генерируется хеш
         $hash = md5(generateHash(10));
-        //Пароль из формы записывается в переменную
-        $parol = trim($_POST["parol"]);
         // Окончательный пароль, состоящий из пароля+хеша
         $hashparol = md5($parol.$hash);
         //Добавить в таблицу с юзерами шифрованный пароль, почту и хеш для дальнейшей расшифровки
-        mysqli_query($link," INSERT INTO users SET user_email='".$email."', user_parol='".$hashparol."', user_hash='".$hash."' " );
+        mysqli_query($link," INSERT INTO users SET user_email='".$email."', user_phone='".$phone."' , user_parol='".$hashparol."', user_hash='".$hash."', user_data ='".$date."' " );
         //Отравить письмо об успешной регистрации
         require_once "mail_register.php";
         //Перейти на страницу с логином
-        header("Location: login.php"); exit();
+        header("Location: vhod.php"); exit();
     }
     //Либо вывести ошибку
     else
@@ -80,24 +99,44 @@ if(isset($_POST["submit"])) {
 	<title>Регистрация на сайте goravtobaza</title>
 
 		<!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-	<!-- Optional JavaScript -->
+      <link href="css/css/bootstrap.min.css" rel="stylesheet">
+      <!-- Users CSS -->
+      <link rel="stylesheet" type="text/css" href="css/css/registraciya.css">
+    <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.min.js"></script>
 </head>
 <body>
+
+    <!-- Навигация -->
+    <nav class="col-md-12 ">
+        <div class="container">
+            <div class="row">
+            <ul class="list-inline col-md-12">
+                <a href="http://goravtobaza.ru/index.html"><li "> <img class="img-responsive" src="img/logo.png" alt="logo"></li></a>
+                <a href="http://goravtobaza.ru/index.html"><li class="col-xl-2 col-lg-3 col-sm-12 btn btn-outline-light">Главная</li></a>
+                <a href="#"><li class="col-xl-2 col-lg-3 col-sm-12 btn btn-outline-light">О проекте</li></a>
+                <a href="#"><li class="col-xl-2 col-lg-3 col-sm-12 offset-xl-0 offset-lg-5 btn btn-outline-light">Помощь</li></a>
+                <a href="http://goravtobaza.ru/registraciya_ili_vhod.html"><li class="col-xl-2 col-lg-3 col-sm-12 btn btn-outline-light">Подать объявление</li></a>
+            </ul>
+            </div>
+        </div>
+    </nav>
+    
 
 <form method="POST" class="row">
 	<div class="form-group, col-sm-2, offset-md-5">
     		<p> <label for="email"> Email: </p> </label>  <input class="form-control" name="email" type="email" id="email" required><br>
+            <p> <label for="phone"> Номер телефона </p> </label>  <input class="form-control" name="phone" type="tel" id="phone" required><br>
     		<p> <label for="parol">  Пароль: </p> </label> <input class="form-control" name="parol" type="password" id="parol" required><br>
-    	  <!--  <p> <label for="captcha"> Введите капчу: </label> </p>
+            <p> <label for="povtor_parol"> Повторите ваш пароль: </p> </label> <input class="form-control" name="povtor_parol" type="password" id="povtor_parol" required><br>
+    	<!--    <p> <label for="captcha"> Введите капчу: </label> </p>
     		<p> <img src="captcha.php" alt="Капча"/> </p> 
-    		<p> <input type="text" name="captcha" id="captcha" placeholder="Проверочный код" required> </p> -->
-            <input type="checkbox" name="zapomnit">
-    		<p> <input class="btn btn-primary" name="submit" type="submit" value="Зарегистрироваться"> </p>
+    		<p> <input type="text" name="captcha" id="captcha" placeholder="Проверочный код" required> </p> 
+           <input type="checkbox" name="zapomnit"> -->
+    		<p> <input class="btn btn-warning" name="submit" type="submit" value="Зарегистрироваться"> </p>
 	</div>
 </form>
 
--- vhod.php
+++ vhod.php
@@ -3,11 +3,11 @@
 // Подключения модуля соединения с БД
 require_once "podklyuchenie_k_bd.php";
 
-if (isset($_POST["submit"])) {
+if (isset($_REQUEST["submit"])) {
     // Получаем пароль из формы
-    $form_parol = $_POST["parol"];
+    $form_parol = $_REQUEST["parol"];
     // Вытаскиваем из БД айди и пароль, где почта равна введённой в форму авторизации
-    $user_login = mysqli_query($link,"SELECT user_id, user_parol, user_hash FROM users WHERE user_email='".mysqli_real_escape_string($link,$_POST["email"])."' ");
+    $user_login = mysqli_query($link,"SELECT user_id, user_parol, user_hash FROM users WHERE user_email='".mysqli_real_escape_string($link,$_REQUEST["email"])."' ");
     // Данные из БД переводятся в массив
     $dannye = mysqli_fetch_assoc($user_login);
     // Достаём хеш для шифрования текущего пароля
@@ -17,7 +17,7 @@ if (isset($_POST["submit"])) {
     // Сравниваем пароли из БД и из формы авторизации
     if ($user_parol === $hashparol) { 
         // Сохраняем пользователя в куках
-        setcookie("id", $_POST["parol"].$_POST["email"], time()+60*60*24*30);
+        setcookie("id", $_REQUEST["parol"].$_REQUEST["email"], time()+60*60*24*30*12);
         //Надо привязать куку к айпи
         // Переводим в личный кабинет
         header("Location: html/lichnyj_kabinet.html/".$dannye["user_id"]); exit();
@@ -32,22 +32,39 @@ if (isset($_POST["submit"])) {
 <html>
 <head>
     <title>Войти на сайт goravtobaza</title>
-		<!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-	<!-- Optional JavaScript -->
+        <!-- Bootstrap CSS -->
+      <link href="css/css/bootstrap.min.css" rel="stylesheet">
+      <!-- Users CSS -->
+      <link rel="stylesheet" type="text/css" href="css/css/vhod.css">
+    <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.min.js"></script>
 </head>
 
 <body>
+
+    <!-- Навигация -->
+    <nav class="col-md-12 ">
+        <div class="container">
+            <div class="row">
+            <ul class="list-inline col-md-12">
+                <a href="http://goravtobaza.ru/index.html"><li "> <img class="img-responsive" src="img/logo.png" alt="logo"></li></a>
+                <a href="http://goravtobaza.ru/index.html"><li class="col-xl-2 col-lg-3 col-sm-12 btn btn-outline-light">Главная</li></a>
+                <a href="#"><li class="col-xl-2 col-lg-3 col-sm-12 btn btn-outline-light">О проекте</li></a>
+                <a href="#"><li class="col-xl-2 col-lg-3 col-sm-12 offset-xl-0 offset-lg-5 btn btn-outline-light">Помощь</li></a>
+                <a href="http://goravtobaza.ru/registraciya_ili_vhod.html"><li class="col-xl-2 col-lg-3 col-sm-12 btn btn-outline-light">Подать объявление</li></a>
+            </ul>
+            </div>
+        </div>
+    </nav>
 
 	<form class="row" method="POST">
         <div class="form-group, col-sm-2, offset-md-5">
     		<p> <label for="email"> Email: </p> </label>  <input class="form-control" name="email" type="email" id="email" required><br>
     		<p> <label for="parol">  Пароль: </p> </label> <input class="form-control" name="parol" type="password" id="parol" required><br>
-    		<input class="btn btn-primary" name="submit" type="submit" value="Войти">
+    		<input class="btn btn-warning" name="submit" type="submit" value="Войти">
         </div>
 	</form>
 
